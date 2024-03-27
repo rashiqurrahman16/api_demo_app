359f40561fb0289994debf6cd3c4bf4d0bade0ba
rashiqurrahman16 <rashiqur.rahman16@gmail.com>
2024-03-27 15:51:53 +0600

    api working

---
+++ lib/homepage.dart
@@ -0,0 +1,91 @@
+
+import 'dart:convert';
+
+import 'package:api_demo_app/model/postModel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async{
+    final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+    var data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200){
+
+      for(Map i in data){
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    }
+    else{
+      return postList;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api Demo'),
+        centerTitle: true,
+        backgroundColor: Colors.black54,
+        foregroundColor: Colors.white,
+      ),
+
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<PostModel>> snapshot) {
+                  if(!snapshot.hasData){
+                    return Center(
+                      child: Text("Loading"),
+                    );
+                  }
+                  else{
+                    return ListView.builder(
+                      itemCount: postList.length,
+                        itemBuilder: (context, index){
+                          return Column(
+                            children: [
+                              Card(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        Text("Id: "+postList[index].id.toString()),
+                                        Text("Title: "+postList[index].title.toString()),
+                                        Text("Body: "+postList[index].body.toString()),
+                                      ],
+                                    ),
+                                  )
+                              ),
+                            ],
+                          );
+                        }
+                    );
+                  }
+              },
+            
+            ),
+          )
+        ],
+      ),
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:api_demo_app/homepage.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Api Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
